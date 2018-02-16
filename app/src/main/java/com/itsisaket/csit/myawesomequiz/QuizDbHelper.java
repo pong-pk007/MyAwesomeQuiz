802a556e72454d20bb76b62944d30a30f0e13ac5
Pongsathon Somjai <pongsathon_pk_202@hotmail.com>
2018-02-16 09:06:47 +0700

    ep.3 SQLITE OPEN HELPER CLASS

---
+++ app/src/main/java/com/itsisaket/csit/myawesomequiz/QuizDbHelper.java
@@ -0,0 +1,81 @@
+package com.itsisaket.csit.myawesomequiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.itsisaket.csit.myawesomequiz.QuizContract.*;
+
+/**
+ * Created by Pong_pk007 on 16/02/2561.
+ */
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME  = "MyAwesomeQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
+
+    public QuizDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionTable();
+
+
+    }
+
+    private void fillQuestionTable() {
+        Question q1 = new Question("A is correct", "A", "B", "C", 1);
+        addQuestion(q1);
+        Question q2 = new Question("B is correct", "A", "B", "C", 2);
+        addQuestion(q2);
+        Question q3 = new Question("C is correct", "A", "B", "C", 3);
+        addQuestion(q3);
+        Question q4 = new Question("A is correct again", "A", "B", "C", 1);
+        addQuestion(q4);
+        Question q5 = new Question("B is correct again", "A", "B", "C", 2);
+        addQuestion(q5);
+
+    }
+
+    private void addQuestion(Question question){
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+
+        onCreate(db);
+
+    }
+}
